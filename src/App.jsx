d03e4ede36da325be535da76dbe1afe03083bdd5
biancamaciel2021 <bianca.maciel@valesulshopping.com.br>
2022-01-19 11:55:58 -0300

    Webpack and Sass configuration

---
+++ src/App.jsx
@@ -0,0 +1,6 @@
+import './styles/global.scss';
+import { RepositoryList } from './components/RepositoryList';
+
+export function App(){
+    return <RepositoryList />
+}
---
+++ src/components/RepositoryList.jsx
@@ -0,0 +1,12 @@
+export function RepositoryList(){
+    return (
+        <section className="repository-list">
+            <h1>Repository List</h1>
+            <ul>
+                <li>unform</li>
+                <p>Forms in React</p>
+                <a href="">Go to repository</a>
+            </ul>
+        </section>
+    )
+}
